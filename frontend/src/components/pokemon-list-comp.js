72b9a1eda497ac41148a2be10f208960da268943
Zyrus <zbwallit@up.edu.ph>
2023-06-17 23:31:47 +0800

    feat: added pagination

--- frontend/src/components/pokemon-list-comp.js
+++ frontend/src/components/pokemon-list-comp.js
@@ -1,22 +1,26 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import InfiniteScroll from 'react-infinite-scroll-component';
 import PokemonCard from './pokemon-card-comp';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 
 function PokemonList() {
+    const [pokemonSizeList, setpokemonSizeList] = useState(0);
     const [pokemonList, setPokemonList] = useState([]);
+    const [pokemonListPage, setPokemonListPage] = useState(0);
 
     useEffect(() => {
         fetchPokemonList();
     }, []);
 
     const fetchPokemonList = async () => {
+        setPokemonListPage(pokemonListPage + 1);
+        console.log(pokemonListPage)
         try {
-            const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=10');
+            const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=10&offset=' + (10*pokemonListPage));
             const data = await response.json();
-
-            setPokemonList(data.results);
-            
+            setpokemonSizeList(data.count);
+            setPokemonList([...pokemonList, ...data.results]);
         } catch (error) {
             console.error('Failed to fetch data:', error.message);
         }
@@ -24,13 +28,20 @@ function PokemonList() {
 
     return (
         <div>
-            <Row xs={1} md={5}>
-            {pokemonList.map((pokemon, index) => (
-                <Col key={index}>
-                    <PokemonCard pokemon={pokemon.url}/>
-                </Col>
-            ))}
+            <InfiniteScroll
+                dataLength={pokemonList.length}
+                next={fetchPokemonList}
+                hasMore={pokemonList.length < pokemonSizeList}
+            >
+                <Row xs={1} md={5}>
+                    {pokemonList.map((pokemon, index) => (
+                        <Col key={index}>
+                            <PokemonCard pokemon={pokemon.url}/>
+                        </Col>
+                    ))}
+                
             </Row>
+            </InfiniteScroll>
         </div>
     );
 }
